76b811c900cdd31179084f78d5280a939d837bd7
gcr2000e <a22409619@alunos.ulht.pt>
2025-05-08 16:44:42 +0100

    Add FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string filename = args[0];
+
+            Console.WriteLine("Insira texto (linha vazia para terminar):");
+
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input))
+                        break;
+
+                    writer.WriteLine(input); // Escreve diretamente no ficheiro
+                }
+            }
+
+            Console.WriteLine($"Texto guardado em \"{filename}\" com sucesso.");
+        }
+    }
+}
