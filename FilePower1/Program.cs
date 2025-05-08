482fb1430ed870941baa87f6ead482e603b719ca
gcr2000e <a22409619@alunos.ulht.pt>
2025-05-08 16:37:02 +0100

    Add FilePower1 project

---
+++ FilePower1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filename = args[0];
+        Queue<string> linhas = new Queue<string>();
+
+        Console.WriteLine("Insira texto (linha vazia para terminar):");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                break;
+
+            linhas.Enqueue(input);
+        }
+
+        try
+        {
+            File.WriteAllLines(filename, linhas);
+            Console.WriteLine($"Texto guardado em \"{filename}\" com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao escrever no ficheiro: {ex.Message}");
+        }
+    }
+}
